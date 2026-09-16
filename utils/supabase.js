5a51cf5bcbfb0d37ba85a66c92a41ab3ec4ab93c
Seo Yumin <92535647+philip0613@users.noreply.github.com>
2026-09-16 19:20:15 +0900

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// 복잡한 환경변수 설정 없이 알려주신 키를 직접 넣었습니다.
+const supabaseUrl = 'https://bmegkgonjdektvywwnkx.supabase.co';
+const supabaseKey = 'sb_publishable_Adv3RXQsXuA-sQT2Nudq-w_HbyIZ893';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
