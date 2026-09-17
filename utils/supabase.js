bd5bd94bbb2d1338d81e2b1104445b2a2cbe39ce
Seo Yumin <92535647+philip0613@users.noreply.github.com>
2026-09-17 14:18:32 +0900

    supabase.js 삭제

--- utils/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://bmegkgonjdektvywwnkx.supabase.co';
-const supabaseKey = 'sb_publishable_Adv3RXQsXuA-sQT2Nudq-w_HbyIZ893';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
